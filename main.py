2f6a905b2ef41054ff03167e58004853798a9052
RoryFotheringham <roryabfotheringham@gmail.com>
2022-02-18 17:59:56 +0000

    restructuring

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-from geojson import FeatureCollection, Feature, LineString, Point
-import geojson
-import os
-#class is representation of boundary and known obstacles in the park
-class Boundaries:
-
-    def __init__(self):
-        self.boundaryCoordinates = 0  # list of points
-        self.obstacles = 0
-        self.readBoundaries()
-
-    def readBoundaries(self):
-        with open('Maps/testMap.geojson') as file:
-            gj = geojson.load(file)
-            print(gj)
-            self.boundaryCoordinates = gj['features'][0]
+from Boundaries import Boundaries
 
 
 # Press the green button in the gutter to run the script.

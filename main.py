aecb08625115a0c87801357d5bbf1f79629aace1
RoryFotheringham <roryabfotheringham@gmail.com>
2022-02-20 16:18:13 +0000

    created longlat class and is_valid_point method

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from Boundaries import Boundaries
+from boundaries import Boundaries
+from long_lat import LongLat
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     G = Boundaries('Maps/testMap.geojson')
-    print(G.boundaryCoordinates)
+    print(G.boundary_polygon)
+    print(G.obstacle_polygons)
+    print(G.obstacle_linestring)
+    print(G.boundary_linestring)
+    l = LongLat(1, 1) #false
+    l2 = LongLat(-3.197085857391357,
+          55.94205038972295) #true
+    l3 = LongLat( -3.196130990982055,
+          55.94126327057947) #false
+    l4 = LongLat(-3.197418451309204,
+          55.94164181079011) #false
+    print(G.is_valid_point(l4))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/

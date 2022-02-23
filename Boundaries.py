0cd3edb19b60ec1178fd7e1ed85076864a0380d3
RoryFotheringham <roryabfotheringham@gmail.com>
2022-02-23 17:44:40 +0000

    rewrote class with shapely, boundary_test, generate_points

--- Boundaries.py
+++ Boundaries.py
@@ -1,64 +1,85 @@
-from geojson import FeatureCollection, Feature, LineString, Point
-import geojson
-from turfpy.measurement import boolean_point_in_polygon
-
-from turfpy.misc import line_intersect
+import json
+from shapely.geometry import Point, Polygon, LineString, shape, MultiPolygon
 import copy
-import long_lat
+from long_lat import LongLat
 from typing import List
-
+import math
 
 # class is representation of boundary and known obstacles in the park
 class Boundaries:
 
     def __init__(self, fname):
 
-        gj = self.read_file(fname)
-        self.boundary_polygon = gj['features'][0]   # polygon
-        self.obstacle_polygons = gj['features'][1:]  # list of polygons
-        self.boundary_linestring = self.polygon_to_linestring(self.boundary_polygon)
-        self.obstacle_linestring = [self.polygon_to_linestring(polygon) for polygon in self.obstacle_polygons]
+        multi_polygon = self.read_file(fname)
 
+        self.boundary_polygon = multi_polygon.geoms[0]
+        self.obstacle_polygon = multi_polygon.geoms[1:]
+        print(self.boundary_polygon)
+        print("\n")
+        print(self.obstacle_polygon)
+
+
+    # method reads geojson file and returns a shapely.geometry.MultiPolygon object
     def read_file(self, fname):
         with open(fname) as file:
-            gj = geojson.load(file)
-            return gj
-
-    def polygon_to_linestring(self, polygon):
-        linestring = copy.deepcopy(polygon)
-        linestring['geometry']['type'] = 'LineString'
-        linestring['geometry']['coordinates'] = linestring['geometry']['coordinates'][0]
-        return linestring
+            features = (json.load(file)["features"])
+            multi_polygon = MultiPolygon([shape(feature["geometry"]) for feature in features])
+            return multi_polygon
 
 #todo isIntersecting, geojson polygon, generate grid
 
     # method returns true iff point is in the boundary and in none of the obstacles
     def is_valid_point(self, longLat):
         point = longLat.to_point()
-        return boolean_point_in_polygon(point, self.boundary_polygon) and\
-            not(self.point_in_polygons(self.obstacle_polygons, point))
+        return self.boundary_polygon.contains(point) and not(self.obstacle_polygon.contains(point))
 
+    # iterates over multiple polygons, returns true if the given point is contained in any of them
     def point_in_polygons(self, polygons, point):
         for poly in polygons:
-            if boolean_point_in_polygon(point, poly):
+            if poly.contains(point):
                 return True
         return False
 
-
-    # method checks if line made from two longlat points intersects any boundary or obstacle
+    # checks if a line made from two longlat points crosses the border of the boundary or any obstacle
     def is_intersecting(self, longLat1, longLat2):
         linestring = longLat1.to_LineString(longLat2)
-        print(line_intersect(linestring, self.boundary_linestring))
+        return linestring.crosses(self.boundary_polygon) or linestring.crosses(self.obstacle_polygon)
 
-
-
-    def isValidEdge(self, longLat1, longLat2):
+    def is_valid_edge(self, longLat1, longLat2):
         ...
 
 
-    def generatePoints() -> List[long_lat.LongLat]:
-        #at least length of robot
-        ...
-        
-    def crossesBoundaries():
-        ...
+    def generate_points(self):
+        #robot_size_in_coords = 0.0000027027
+        robot_size_in_coords = 0.0001
+        x1_bound = self.boundary_polygon.bounds[0]
+        y1_bound = self.boundary_polygon.bounds[1]
+        x2_bound = self.boundary_polygon.bounds[2]
+        y2_bound = self.boundary_polygon.bounds[3]
+
+        # init width and height of the array
+        width = math.floor((x2_bound - x1_bound)/robot_size_in_coords)
+        height = math.floor((y2_bound - y1_bound)/robot_size_in_coords)
+
+        x = x1_bound
+        y = y2_bound
+
+        grid = [[-999 for j in range(width)] for i in range(height)] # init array
+
+        # adds LongLat point to the grid if it is a valid point
+        for i in range(height):
+            if i != 0:
+                y = y - robot_size_in_coords
+            x = x1_bound
+            for j in range(width):
+                if j != 0:
+                    x = x + robot_size_in_coords
+                point = LongLat(x,y)
+                if self.is_valid_point(point):
+                    grid[i][j] = LongLat(x, y)
+
+        print(grid)
+        print(len(grid[0]))
+        print(x1_bound)
+        print(y2_bound)
+
--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-from boundaries import Boundaries
+from Boundaries import Boundaries
 from long_lat import LongLat
+from boundary_test import BoundaryTest
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     G = Boundaries('Maps/testMap.geojson')
-    print(G.boundary_polygon)
-    print(G.obstacle_polygons)
-    print(G.obstacle_linestring)
-    print(G.boundary_linestring)
-    l = LongLat(1, 1) #false
-    l2 = LongLat(-3.197085857391357,
-          55.94205038972295) #true
-    l3 = LongLat( -3.196130990982055,
-          55.94126327057947) #false
-    l4 = LongLat(-3.197418451309204,
-          55.94164181079011) #false
-    l5 = LongLat( -3.195412158966064,
-          55.9416237851497) #outside boundary
-    print(G.is_intersecting(l4, l5))
 
+    valid_point1 = LongLat(-3.19627046585083,55.941780007088)
+    valid_point2 = LongLat(3.196860551834106,55.94188816036847)
+    valid_point3 = LongLat(-3.1984376907348633,55.94196627088323)
+
+    G.generate_points()

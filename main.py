e3e407d0207b4b4d0bffe2405d483f49d8d83d88
RoryFotheringham <roryabfotheringham@gmail.com>
2022-02-27 17:05:04 +0000

    implemented buffer boundary, restructered code wrt new buffer

--- main.py
+++ main.py
@@ -10,5 +10,4 @@ if __name__ == '__main__':
     valid_point1 = LongLat(-3.19627046585083,55.941780007088)
     valid_point2 = LongLat(3.196860551834106,55.94188816036847)
     valid_point3 = LongLat(-3.1984376907348633,55.94196627088323)
-
-    G.generate_points()
+    G.show_plot()

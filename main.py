b1ff7992ecbd869125419b3f0bed60a18a958e71
RoryFotheringham <roryabfotheringham@gmail.com>
2022-03-02 16:15:51 +0000

    formatting

--- main.py
+++ main.py
@@ -15,12 +15,14 @@ if __name__ == '__main__':
     valid_point3 = LongLat(-3.1984376907348633,55.94196627088323)
 
     start = LongLat(-2.65869140625,
-          1.1864386394452024)
+                    1.1864386394452024)
+
     end = LongLat (-0.19775390625,
-          2.4272521703917294)
+                    2.4272521703917294)
 
     constraints = [(LongLat(-0.9000000000000004,3.1000000000000005),
-                                         LongLat(-0.9000000000000004,2.8000000000000007))]
+                    LongLat(-0.9000000000000004,2.8000000000000007))]
+
    # constraints = []
     
     search = Graph(G)
@@ -36,7 +38,7 @@ if __name__ == '__main__':
 
     plot_line_strings = []
     for i in range(len(path) - 1):
-        plot_line_strings.append((path[i].longLat).to_LineString(path[i+1].longLat))
+        plot_line_strings.append(path[i].longLat.to_LineString(path[i + 1].longLat))
         
     for ls in plot_line_strings:
         x,y = ls.xy

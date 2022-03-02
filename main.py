5c2f5fcc85a54d42be0e968076f9d434e80d9ac6
RoryFotheringham <roryabfotheringham@gmail.com>
2022-03-02 16:12:37 +0000

    improved constraint plotting, fixed bugs respectively

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from boundary_test import BoundaryTest
 import matplotlib.pyplot as plt
 
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     G = Boundaries('Maps/sdp_space_map.geojson')
@@ -17,10 +18,22 @@ if __name__ == '__main__':
           1.1864386394452024)
     end = LongLat (-0.19775390625,
           2.4272521703917294)
+
+    constraints = [(LongLat(-0.9000000000000004,3.1000000000000005),
+                                         LongLat(-0.9000000000000004,2.8000000000000007))]
+   # constraints = []
     
     search = Graph(G)
-    path = (search.GetPath(start =start, end=end))
-    
+    path = (search.GetPath(start =start, end=end,
+                           constraints=constraints))
+
+    if constraints:
+        for pair in constraints:
+            midpoint = pair[0].midpoint(pair[1])
+            plt.scatter(midpoint.long, midpoint.lat, marker='H', color='red', s=300)
+
+
+
     plot_line_strings = []
     for i in range(len(path) - 1):
         plot_line_strings.append((path[i].longLat).to_LineString(path[i+1].longLat))
@@ -28,6 +41,6 @@ if __name__ == '__main__':
     for ls in plot_line_strings:
         x,y = ls.xy
         plt.plot(x,y, color="#ff7040", linewidth=2, solid_capstyle='round')
-        
+
     G.show_plot()
         

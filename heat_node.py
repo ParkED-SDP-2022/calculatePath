90733d51a98f1fc4ca616d7929c360b58e6bb384
Abdul Rehman <arehmannasir@outlook.com>
2022-04-06 08:25:02 +0100

    updated heat node

--- heat_node.py
+++ heat_node.py
@@ -9,30 +9,22 @@ class HeatNode:
     def __init__(self):
         self.heat_map = HeatMapList(3)
         rospy.init_node('heat_node', anonymous=True)
-        # # subscribes to pressure sensor data. The payload of the the message will just be a boolean
-        # self.bench1_pressure_subscriber = rospy.Subscriber('/bench_1_pressure_data', Bool,
-        #                                             self.pressure_subscriber_cb, callback_args=[1])
-        # self.bench2_pressure_subscriber = rospy.Subscriber('/bench_2_pressure_data', Bool,
-        #                                             self.pressure_subscriber_cb, callback_args=[2])
-        # self.bench3_pressure_subscriber = rospy.Subscriber('/bench_3_pressure_data', Bool,
-        #                                             self.pressure_subscriber_cb, callback_args=[3])
-        # self.bench4_pressure_subscriber = rospy.Subscriber('/bench_4_pressure_data', Bool,
-        #                                             self.pressure_subscriber_cb, callback_args=[4])
+
+        self.bench1_pressure_data_pub = rospy.Publisher('/bench1_pressure_data', Bool)
+        self.bench1_pressure_data = Bool()
 
         # subscribes to robot's current position. The payload of the message will be a parked_custom_msgs/Point object
         self.position_subscriber = rospy.Subscriber('/robot_position_longlat', Point, self.update_current_position, callback_args=1)
         self.position_subscriber = rospy.Subscriber('/bench2_position_longlat', Point, self.update_current_position, callback_args=2)
-        # self.position_subscriber = rospy.Subscriber('/robot_positon_3', Point, self.update_current_position, callback_args=3)
+        self.position_subscriber = rospy.Subscriber('/bench3_position_longlat', Point, self.update_current_position, callback_args=3)
         # self.position_subscriber = rospy.Subscriber('/robot_positon_4', Point, self.update_current_position, callback_args=4)
         self.bench1_current_position = Point(0.6,0.6,-999)
         self.bench2_current_position = Point(1.6,1.7,-999)
         self.bench3_current_position = Point(-0.3,-0.2,-999)
-        self.bench4_current_position = Point(0.5,0.5,-999)
 
         self.bench1_occupied = False
         self.bench2_occupied = False
         self.bench3_occupied = False
-        self.bench4_occupied = False
 
         # publishes string notation of geojson
         self.heat_map_publisher = rospy.Publisher('/heat_map_data', String, queue_size=5)
@@ -72,9 +64,6 @@ class HeatNode:
             self.publisher_msg.data = geojson_heatmap
             self.heat_map_publisher.publish(self.publisher_msg)
             
-        # geojson_benchstate = str(self.heat_map.to_geojson_benchstate())
-        # self.bench_state_publisher_msg.data = geojson_benchstate
-        # self.bench_state_publisher.publish(self.bench_state_publisher_msg)
     
     # Updates stored bench positions when they change and publishes new feature collection of benches to frontend
     # TODO: ensure this isn't publishing too often to be feasible
@@ -89,6 +78,8 @@ class HeatNode:
             if position.angle == -999 and not self.bench1_occupied:
                 self.bench1_occupied = True
                 self.update_heatmap(self.bench1_occupied, 1)
+                self.bench1_pressure_data.data = self.bench1_occupied
+                self.bench1_pressure_data_pub.publish(self.bench1_pressure_data)
             elif position.angle != -999 and self.bench1_occupied:
                 self.bench1_occupied = False
                 self.update_heatmap(self.bench1_occupied, 1)
@@ -104,11 +95,16 @@ class HeatNode:
                 self.bench2_current_position = position
                 self.heat_map.change_loc(2, position)
         elif bench_id == 3:
-            self.bench3_current_position = data
-            self.heat_map.change_loc(3, data)
-        elif bench_id == 4:
-            self.bench4_current_position = data
-            self.heat_map.change_loc(4, data)
+            if position.long == 0 and position.lat == 0 and not self.bench3_occupied:
+                self.bench3_occupied = True
+                self.update_heatmap(self.bench3_occupied, 3)
+            elif (position.long != 0 or position.lat != 0) and self.bench3_occupied:
+                self.bench3_occupied = False
+                self.update_heatmap(self.bench3_occupied, 3)
+            elif (position.long != 0 or position.lat != 0) and (position.long != self.bench3_current_position.long or position.lat != self.bench3_current_position.lat):
+                print("updating pos")
+                self.bench3_current_position = position
+                self.heat_map.change_loc(3, position)
             
         geojson_benchstate = str(self.heat_map.to_geojson_benchstate())
         self.bench_state_publisher_msg.data = geojson_benchstate
